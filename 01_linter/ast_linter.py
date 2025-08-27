149112745a1d71b105c6d5c13c764d6abd1e4875
Grzegorz Nowiński <grzesiekn1001@gmail.com>
2025-08-27 09:45:42 +0200

    01_linter - ast_linter with empty visitor

--- 01_linter/ast_linter.py
+++ 01_linter/ast_linter.py
@@ -1,4 +1,12 @@
 from pathlib import Path
 import ast
 
+class Visitor(ast.NodeVisitor):
+    def visit(self, node: ast.AST):
+        self.generic_visit(node)
+
+
 source_code = Path('code_with_problems.py').read_text()
+node = ast.parse(source_code)
+
+Visitor().visit(node)
