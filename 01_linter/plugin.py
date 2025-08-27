839189c1410717e7d45202d9c20fa43279880e1f
Grzegorz Nowiński <grzesiekn1001@gmail.com>
2025-08-27 10:26:42 +0200

    01_linter - plugin implement visitor

--- 01_linter/plugin.py
+++ 01_linter/plugin.py
@@ -9,6 +9,21 @@ class ErrorInfo(NamedTuple):
     cls: type
 
 
+class Visitor(ast.NodeVisitor):
+
+    def __init__(self) -> None:
+        self.errors: list[ErrorInfo] = []
+
+    def visit_FunctionDef(self, node: ast.FunctionDef):
+        for default in node.args.defaults:
+            if isinstance(default, ast.List):
+                self.errors.append(
+                    ErrorInfo(node.lineno, node.col_offset,  "PC25001 Always fail on", type(self))
+                    )
+
+        self.generic_visit(node)
+
+
 class PyConPlugin:
     name = 'pycon_plugin'
     version = '0.0.0'
@@ -17,4 +32,7 @@ class PyConPlugin:
         self._tree = tree
 
     def run(self):
-        yield ErrorInfo(0, 0, "PC25001 Always fail on", type(self))
+        visitor = Visitor()
+        visitor.visit(self._tree)
+
+        yield from visitor.errors
