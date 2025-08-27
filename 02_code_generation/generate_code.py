fc61bab078ea24a2e3bfe5c2340192802d693a01
Grzegorz Nowiński <grzesiekn1001@gmail.com>
2025-08-27 07:45:20 +0200

    02_code_generation - init

---
+++ 02_code_generation/generate_code.py
@@ -0,0 +1,18 @@
+import ast
+import json
+from pathlib import Path
+
+schema = json.loads(Path('schema.json').read_text())
+
+
+def generate_class_ast(class_name: str, fields: dict):
+    ...
+
+def schema_to_ast(schema):
+    return [generate_class_ast(name, fields) for name, fields in schema.items()]
+
+
+tree = ast.Module(body=schema_to_ast(schema), type_ignores=[])
+tree = ast.fix_missing_locations(tree)
+
+print(ast.unparse(tree))
