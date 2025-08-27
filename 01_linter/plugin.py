fce83c31f8f5e4caf101caaba9ee443dcd8c1a98
Grzegorz Nowiński <grzesiekn1001@gmail.com>
2025-08-27 09:51:50 +0200

    01_linter - flake8 always fail on

--- 01_linter/plugin.py
+++ 01_linter/plugin.py
@@ -12,3 +12,9 @@ class ErrorInfo(NamedTuple):
 class PyConPlugin:
     name = 'pycon_plugin'
     version = '0.0.0'
+
+    def __init__(self, tree: ast.AST) -> None:
+        self._tree = tree
+
+    def run(self):
+        yield ErrorInfo(0, 0, "PC25001 Always fail on", type(self))
